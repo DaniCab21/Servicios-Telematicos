0bb87fc7d84886ee722517f3b0fe29d6c23dc5fd
DaniCab21 <danielcabiativa.dc@gmail.com>
2022-09-11 16:11:56 -0500

    Primer commit de este repositorio

---
+++ Ejemplo2_VS/comandos/ejercicio2/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("digite un número");
+int num = Console.Read();
+Console.WriteLine("El ascii es = "+num);
+Console.Clear();
---
+++ Ejercicio1_VS/ejercicio1/ejercicio2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+int [,] Matriz = new int[3,3];
+int j=0;
+for (int i = 0; i < 3; i++){
+    for (j = 0; j < 2; j++)    {
+        Console.WriteLine("Ingrese valor para el compoentente "+i+", "+j);
+        int numero = Convert.ToInt16(Console.ReadLine());
+        Matriz[i,j] = numero;        
+    }
+        Matriz[i,j] = Matriz[i,j-2]*Matriz[i,j-1];
+        j=0;
+}
+for (int i = 0; i < 3; i++){
+    for (int k = 0; k < 3; k++){
+        Console.Write(""+Matriz[i,k]);
+    }
+    Console.WriteLine(" ");
+}
